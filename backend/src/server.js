4078de1b1f098d16d1b8895ba759c2983a79f502
Upayan Naskar <upayannaskar2009@outlook.com>
2025-08-29 23:06:48 +0530

    initial commit

---
+++ backend/src/server.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import { ENV } from './config/env.js';
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(ENV.PORT, () => {
+  console.log('Server is running on port', ENV.PORT);
+});
