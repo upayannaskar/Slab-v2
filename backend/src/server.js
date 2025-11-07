4bdded19a3ca7abc1591b4ce1d835e14044294d7
Upayan Naskar <upayannaskar2009@outlook.com>
2025-11-07 22:33:44 +0530

    problem resolved

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,14 +1,33 @@
 import express from 'express';
 import { ENV } from './config/env.js';
 import { connectDB } from './config/db.js';
+import { clerkMiddleware } from '@clerk/express';
+import { functions, inngest } from './config/inngest.js';
+import { serve } from "inngest/express";
 
 const app = express();
+
+app.use(express.json());
+app.use(clerkMiddleware())
+
+app.use("/api/inngest", serve({ client: inngest, functions }));
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.listen(ENV.PORT, () => {
-  console.log('Server is running on port', ENV.PORT);
-  connectDB();
-});
+const startServer = async () => {
+  try {
+    await connectDB();
+    if (ENV.NODE_ENV !== "production") {
+      app.listen(ENV.PORT, () => {
+        console.log('Server is running on port', ENV.PORT);
+      });
+    }
+  } catch (error) {
+    console.error('Failed to start server:', error);
+    process.exit(1);
+  }
+}
+
+export default app
