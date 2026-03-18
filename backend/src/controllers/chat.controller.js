c99760421e458c77cd73294322da981d9309e5db
upayannaskar5 <upayannaskar5@gmail.com>
2026-03-18 08:46:47 +0000

    stream and sentry setup done

---
+++ backend/src/controllers/chat.controller.js
@@ -0,0 +1,14 @@
+import { generateStreamToken } from "../config/stream.js"
+
+export const getStreamToken = async (req, res) => {
+    try {
+        const token = generateStreamToken(req.auth().userId)
+
+        res.status(200).json({token})
+    } catch (error) {
+        console.log("Error generating Stream token:", error)
+        res.status(500).json({
+            message: "Error generating Stream token"
+        })
+    }
+}
