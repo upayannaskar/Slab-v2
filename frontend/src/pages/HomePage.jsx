621b4c71488bf982911379dfd52f37bc8b23c33e
upayannaskar5 <upayannaskar5@gmail.com>
2026-03-21 12:45:47 +0000

    /

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -65,7 +65,7 @@ const HomePage = () => {
                   </span>
                 </div>
 
-                <div className="rounded-full p-1 backdrop-blur-md">
+                <div className="rounded-full bg-transparent p-1 backdrop-blur-md">
                   <UserButton />
                 </div>
               </div>
