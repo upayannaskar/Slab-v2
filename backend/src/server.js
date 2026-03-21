8a04e7f6e685b5d112de5d4e752f30e36023452c
upayannaskar5 <upayannaskar5@gmail.com>
2026-03-21 11:37:50 +0000

    cors

--- backend/src/server.js
+++ backend/src/server.js
@@ -14,7 +14,12 @@ import * as Sentry from "@sentry/node";
 const app = express();
 
 app.use(express.json());
-app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }));
+app.use(
+  cors({
+    origin: "https://slab-v2-fr.vercel.app",
+    credentials: true,
+  })
+);
 app.use(clerkMiddleware()); // req.auth will be available in the request object
 
 app.get("/debug-sentry", (req, res) => {
